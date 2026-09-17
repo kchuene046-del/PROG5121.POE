8bf75521e0813c1b5cb39b01ea58d1de4b6597c9
kchuene-del <kchuene046@gmail.com>
2026-09-17 16:40:01 +0200

    Created check username function

--- PROG5121_POE/PROG5121_POE/PROG5121_POE/src/main/java/Register_Login.java
+++ PROG5121_POE/PROG5121_POE/PROG5121_POE/src/main/java/Register_Login.java
@@ -10,4 +10,37 @@
 public class Register_Login {
     
     
+    
+    
+    public static boolean checkFirstName(String firstName){
+        
+        //check if both first and last name are provided
+        if (!firstName.isEmpty())
+        {
+            return true;
+        }
+       
+            return false;
+    }
+    
+     public static boolean checkLastName(String lastName){
+        
+        //check if both first and last name are provided
+        if (!lastName.isEmpty())
+        {
+            return true;
+        }
+       
+            return false;
+    }
+      public static boolean checkUserName(String userName) {
+         //checks if the user name has an underscore or is less than 5 characters 
+         if(userName != null && userName.contains("_") && userName.length()<= 5)  {
+            return true;
+         }
+         return false;
+    }
+    
+    
 }
+   
